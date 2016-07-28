dca550870a350b6133ddc55442d6929d972e0965
henianming <1500923374@qq.com>
2016-07-28 09:23:02 +0800

    整理创建窗口

--- dragon_lesson_extra.cpp
+++ dragon_lesson_extra.cpp
@@ -2,82 +2,37 @@
 #ifdef DRAGON_LESSON_EXTRA
 
 #include <windows.h>
-#include <iostream>
-using namespace std;
 
-HWND MainWindowHandle = 0;
-
+/*------------------------------------------------------------------------------
+hWnd:该消息所属窗口
+msg:消息的ID
+wParam:消息的参数，内容由消息ID决定，具体需查阅MSDN
+lParam:消息的参数，内容由消息ID决定，具体需查阅MSDN
+------------------------------------------------------------------------------*/
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	switch (msg) {
-	case WM_LBUTTONDOWN:
-		::MessageBox(0, L"Hello, World", L"Hello", MB_OK);
-		return 0;
-	case WM_KEYDOWN:
-		if (wParam == VK_ESCAPE) {
-			::DestroyWindow(MainWindowHandle);
+	case WM_KEYUP: //虚拟按键消息，总为大写
+		switch (wParam) {
+		case 'A':
+			MessageBox(hWnd, L"WM_KEYUP", L"msg", MB_OK);
+			break;
+		case VK_ESCAPE:
+			DestroyWindow(hWnd);
+			break;
 		}
-		if (wParam == 'A') {
-			::MessageBox(0, L"Hello, World", L"Hello", MB_OK);
+		break;
+	case WM_CHAR: //翻译后的字符消息，是否大写由输入决定
+		switch (wParam) {
+		case 'b':
+			MessageBox(hWnd, L"WM_CHAR", L"msg", MB_OK);
+			break;
 		}
-		return 0;
-	case WM_CHAR:
-		if (wParam == 'a') {
-			::MessageBox(0, L"CHAR", L"Hello", MB_OK);
-		}
-		return 0;
+		break;
 	case WM_DESTROY:
-		::PostQuitMessage(0);
-		return 0;
+		PostQuitMessage(0);
+		break;
 	}
-
-	return ::DefWindowProc(hWnd, msg, wParam, lParam);
-}
-
-bool InitWindowsApp(HINSTANCE hInstance, int show) {
-	WNDCLASS wc;
-
-	wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = ::LoadIcon(0, IDI_APPLICATION);
-	wc.hCursor = ::LoadCursor(0, IDC_ARROW);
-	wc.hbrBackground = static_cast<HBRUSH>(::GetStockObject(WHITE_BRUSH));
-	wc.lpszMenuName = 0;
-	wc.lpszClassName = L"Hello";
-
-	if (!::RegisterClass(&wc)) {
-		::MessageBox(0, L"RegisterClass - Failed", 0, 0);
-		return false;
-	}
-
-	MainWindowHandle = ::CreateWindow(L"Hello", L"Hello",
-									  WS_OVERLAPPEDWINDOW,
-									  CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-									  0, 0, hInstance, 0);
-
-	if (MainWindowHandle == 0) {
-		::MessageBox(0, L"CreateWindow - Failed", 0, 0);
-		return false;
-	}
-
-	::ShowWindow(MainWindowHandle, show);
-	::UpdateWindow(MainWindowHandle);
-
-	return true;
-}
-
-int Run() {
-	MSG msg;
-	::ZeroMemory(&msg, sizeof(MSG));
-
-	while (::GetMessage(&msg, 0, 0, 0)) {
-		::TranslateMessage(&msg);
-		::DispatchMessage(&msg);
-	}
-
-	return msg.wParam;
+	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
 /*------------------------------------------------------------------------------
@@ -90,12 +45,50 @@ nCmdShow:指定应用程序窗口的显示形式
 		 SW_SHOWMAXIMIZED:以最大化运行
 ------------------------------------------------------------------------------*/
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hInstancePrev, LPSTR lpCmdLine, int nCmdShow) {
-	if (!InitWindowsApp(hInstance, nCmdShow)) {
-		::MessageBox(0, L"Init - Failed", L"Error", MB_OK);
+	//定义一个窗口类
+	WNDCLASS wc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH));
+	wc.hCursor = LoadCursor(0, IDC_ARROW);
+	wc.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszClassName = L"class name";
+	wc.lpszMenuName = 0;
+	wc.style = CS_VREDRAW | CS_HREDRAW;
+
+	//注册这个窗口类
+	if (!RegisterClass(&wc)) {
+		MessageBox(0, L"RegisterClass Failed!", L"Error", MB_OK);
 		return 0;
 	}
 
-	return Run();
+	//按照这个窗口类创建一个窗口
+	HWND hWnd = CreateWindow(L"class name", L"test window",
+							 WS_OVERLAPPEDWINDOW,
+							 CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+							 0, 0, hInstance, 0);
+
+	//显示这个窗口
+	ShowWindow(hWnd, nCmdShow);
+
+	//消息循环
+	int ret;
+	MSG msg;
+	//从系统捕获消息，只要不是捕获的QUIT信号，均返回非0值
+	while ((ret = GetMessage(&msg, 0, 0, 0)) != 0) {
+		if (ret == -1) { //返回-1，表示消息出错了
+			MessageBox(0, L"Message Error!", L"Error", MB_OK);
+		} else { //正常
+			//翻译虚拟按键消息为字符按键
+			TranslateMessage(&msg);
+			//把捕获的消息发送到对应的窗口过程
+			DispatchMessage(&msg);
+		}
+	}
+
+	return 0;
 }
 
 #endif // DRAGON_LESSON_EXTRA
